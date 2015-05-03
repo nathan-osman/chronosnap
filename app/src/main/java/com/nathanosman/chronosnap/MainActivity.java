77ab5885d1caebb5664bd1466ac3d4db41cd0837
Nathan Osman <nathan@quickmediasolutions.com>
2015-05-02 23:07:44 -0700

    Added preferences activity and a couple of basic preferences.

--- app/src/main/java/com/nathanosman/chronosnap/MainActivity.java
+++ app/src/main/java/com/nathanosman/chronosnap/MainActivity.java
@@ -1,7 +1,8 @@
 package com.nathanosman.chronosnap;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -16,20 +17,16 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
---
+++ app/src/main/java/com/nathanosman/chronosnap/SettingsActivity.java
@@ -0,0 +1,47 @@
+package com.nathanosman.chronosnap;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    @SuppressWarnings("deprecation")
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+
+        bindPreferenceSummaryToValue(findPreference("interval"));
+        bindPreferenceSummaryToValue(findPreference("limit"));
+    }
+
+    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+
+                preference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
+            } else {
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+    };
+
+    private static void bindPreferenceSummaryToValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+
+        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+}
