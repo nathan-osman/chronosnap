59010a36a851b311db5edd9227d2794f367b1d94
Nathan Osman <nathan@quickmediasolutions.com>
2015-05-06 12:12:12 -0700

    Completed initial implementation of ImageCapturer class.

--- app/src/main/java/com/nathanosman/chronosnap/ImageCapturer.java
+++ app/src/main/java/com/nathanosman/chronosnap/ImageCapturer.java
@@ -3,8 +3,12 @@ package com.nathanosman.chronosnap;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.os.AsyncTask;
+import android.os.Environment;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 
 /**
@@ -15,10 +19,12 @@ import java.io.IOException;
  */
 public class ImageCapturer {
 
-    private static final int INVALID_INDEX = -1;
-
     /**
      * Callback interface used to provide notification of capture state
+     *
+     * Both of the callbacks may leave the camera open (to save time when
+     * starting the next capture), so it needs to be explicitly closed when
+     * capturing is finished.
      */
     public interface CaptureCallback {
 
@@ -36,63 +42,166 @@ public class ImageCapturer {
 
     // Data initialized in the constructor
     private int mCameraId;
-    private String mSequenceName;
+    private File mSequencePath;
+
+    // Data initialized by startCapture()
+    private int mIndex;
     private CaptureCallback mCaptureCallback;
 
-    // Data required while communicating with the camera
+    // (Potentially) persistent connection to the camera
     @SuppressWarnings("deprecation")
     private Camera mCamera;
-    private int mCurrentIndex = INVALID_INDEX;
 
     /**
      * Initialize the capturer
      * @param cameraId ID of the camera to use for capturing
      * @param sequenceName user-supplied name for the sequence
      */
-    public ImageCapturer(int cameraId, String sequenceName, CaptureCallback captureCallback) {
+    public ImageCapturer(int cameraId, String sequenceName) {
 
         mCameraId = cameraId;
-        mSequenceName = sequenceName;
-        mCaptureCallback = captureCallback;
+        mSequencePath = new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM),
+                "ChronoSnap" + File.separator + sequenceName
+        );
     }
 
     /**
-     * Begin capture of the specified image
-     * @param index the numerical index of the image to capture
+     * Start capture of the specified image
+     * @param index numerical index of the image to capture
+     * @param captureCallback callback for capture events
      */
-    public void start(int index) {
+    public void startCapture(int index, CaptureCallback captureCallback) {
 
-        mCurrentIndex = index;
+        mIndex = index;
+        mCaptureCallback = captureCallback;
 
-        // Camera.open() needs to be done in a separate task since it blocks
-        new AsyncTask<Void, Void, String>() {
+        // If the camera is already open, we can skip immediately to the
+        // setup step, otherwise, we need to open the camera. It needs to be
+        // done in a separate thread since it may block
+        if (mCamera != null) {
+
+            setup();
+
+        } else {
+
+            new AsyncTask<Void, Void, String>() {
+
+                @Override
+                @SuppressWarnings("deprecation")
+                protected String doInBackground(Void... params) {
+
+                    // Camera.open() throws whenever the camera can't be opened
+                    // and Camera.setPreviewTexture() may throw an error as well
+                    try {
+                        mCamera = Camera.open(mCameraId);
+                        mCamera.setPreviewTexture(new SurfaceTexture(0));
+                        return null;
+                    } catch (RuntimeException | IOException e) {
+                        return e.getMessage();
+                    }
+                }
+
+                @Override
+                protected void onPostExecute(String message) {
+
+                    // If message is non-null, then call CaptureCallback.onError(),
+                    // otherwise start the camera preview and begin autofocus
+                    if (message != null) {
+                        mCaptureCallback.onError(message);
+                        return;
+                    }
+
+                    // Move to the setup step
+                    setup();
+                }
+            };
+        }
+    }
+
+    /**
+     * Close the camera
+     *
+     * Calling this method will require the camera to be re-initialized the
+     * next time that startCapture() is called.
+     */
+    public void close() {
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    /**
+     * Setup the camera in preparation for image capture
+     */
+    @SuppressWarnings("deprecation")
+    private void setup() {
+
+        // The preview needs to be started after each capture
+        mCamera.startPreview();
+        mCamera.autoFocus(new Camera.AutoFocusCallback() {
 
             @Override
-            @SuppressWarnings("deprecation")
-            protected String doInBackground(Void... params) {
+            public void onAutoFocus(boolean success, Camera camera) {
 
-                // Camera.open() throws whenever the camera can't be opened
-                // and Camera.setPreviewTexture() may throw an error as well
+                // TODO: if unable to focus, try again a couple of times
+                // TODO: error message needs to be localized
+
+                // If setup was not successful, we need to report an error
+                if (!success) {
+                    mCaptureCallback.onError("Unable to focus.");
+                    return;
+                }
+
+                // Move to the capture step
+                capture();
+            }
+        });
+    }
+
+    /**
+     * Capture an image
+     */
+    @SuppressWarnings("deprecation")
+    private void capture() {
+
+        mCamera.takePicture(null, null, new Camera.PictureCallback() {
+
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+
+                // Ensure that the destination directory exists and create it otherwise
+                if (!mSequencePath.exists()) {
+
+                    // TODO: error message needs to be localized
+
+                    // Report an error if the directory could not be created
+                    if (!mSequencePath.mkdirs()) {
+                        mCaptureCallback.onError("Unable to create storage directory.");
+                        return;
+                    }
+                }
+
+                // Create the file that will be used for storing the image
+                File jpegFile = new File(mSequencePath, String.format("%04d", mIndex) + ".jpg");
+
+                // Write the data to disk - note that this can fail and result in an error
                 try {
-                    mCamera = Camera.open(mCameraId);
-                    mCamera.setPreviewTexture(new SurfaceTexture(0));
-                    return null;
-                } catch (RuntimeException | IOException e) {
-                    return e.getLocalizedMessage();
-                }
-            }
 
-            @Override
-            protected void onPostExecute(String message) {
+                    OutputStream outputStream = new FileOutputStream(jpegFile);
+                    outputStream.write(data);
+                    outputStream.close();
 
-                // If message is non-null, then call CaptureCallback.onError(),
-                // otherwise start the camera preview
-                if (message == null) {
-                    mCaptureCallback.onError(message);
-                } else {
-                    mCamera.startPreview();
+                } catch (IOException e) {
+
+                    mCaptureCallback.onError(e.getMessage());
+                    return;
                 }
+
+                // Indicate that the capture was successful
+                mCaptureCallback.onSuccess();
             }
-        };
+        });
     }
 }
