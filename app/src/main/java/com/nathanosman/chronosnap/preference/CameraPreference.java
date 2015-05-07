7a8f220b63f1531d25f4e565a937e94259caaae8
Nathan Osman <nathan@quickmediasolutions.com>
2015-05-07 11:02:53 -0700

    Added implementation for default value of CameraPreference (#1).

--- app/src/main/java/com/nathanosman/chronosnap/preference/CameraPreference.java
+++ app/src/main/java/com/nathanosman/chronosnap/preference/CameraPreference.java
@@ -12,22 +12,26 @@ import java.util.List;
 
 
 /**
- * Custom preference type for selecting a hardware camera
+ * Custom preference type for selecting a camera
+ *
+ * Since the number and type of hardware cameras can differ from one model to
+ * the next, the user needs to be able to select which one to use.
  */
 public class CameraPreference extends ListPreference {
 
-    // An invalid index for a camera
-    public static final int INVALID_INDEX = -1;
-
     /**
-     * Reimplementation of ListPreference()
+     * Populate the list of cameras
      */
     @SuppressWarnings("deprecation")
     public CameraPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
 
+        // Create a list of camera names and indices, noting the index of the
+        // rear-facing camera (if discovered) for setting the default
         List<CharSequence> entries = new ArrayList<>();
         List<CharSequence> entryValues = new ArrayList<>();
+
+        CharSequence defaultCamera = null;
 
         // Loop through all of the cameras
         for (int i = 0; i < Camera.getNumberOfCameras(); ++i) {
@@ -37,20 +41,28 @@ public class CameraPreference extends ListPreference {
             Camera.getCameraInfo(i, cameraInfo);
 
             // Record the camera name and index in the arrays
-            switch (cameraInfo.facing) {
-                case Camera.CameraInfo.CAMERA_FACING_FRONT:
-                    entries.add(context.getText(R.string.pref_camera_front));
-                    entryValues.add(String.valueOf(i));
-                    break;
-                case Camera.CameraInfo.CAMERA_FACING_BACK:
-                    entries.add(context.getText(R.string.pref_camera_back));
-                    entryValues.add(String.valueOf(i));
-                    break;
+            if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                entries.add(context.getText(R.string.pref_camera_front));
+            } else {
+                entries.add(context.getText(R.string.pref_camera_back));
+                defaultCamera = String.valueOf(i);
             }
+
+            entryValues.add(String.valueOf(i));
+        }
+
+        // If no suitable default was found, use the first camera
+        if (defaultCamera == null && entryValues.size() != 0) {
+            defaultCamera = entryValues.get(0);
         }
 
         // Add the values to the ListPreference
         setEntries(entries.toArray(new CharSequence[entries.size()]));
         setEntryValues(entryValues.toArray(new CharSequence[entryValues.size()]));
+
+        // Set the default camera if one is present
+        if (defaultCamera != null) {
+            setDefaultValue(defaultCamera);
+        }
     }
 }
